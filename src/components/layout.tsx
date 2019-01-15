c3c086f22697eab70c9123d75ff1ecbdd0fe8248
Kevin Tovar <kevintovar@esimez.mx>
2019-01-14 22:24:24 -0600

    test prettier

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
-import Header from "./header";
-import "../css/style.css";
+import Header from './header'
+import '../css/style.css'
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <StaticQuery
@@ -29,7 +29,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
         <footer className="bg-blue">
           <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
             <p className="text-white">
-              Contactanos en{" "}
+              Contactanos en{' '}
               <a
                 href="https://wa.me/525538434753"
                 className="font-bold no-underline text-white"
@@ -42,10 +42,10 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
       </div>
     )}
   />
-);
+)
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
